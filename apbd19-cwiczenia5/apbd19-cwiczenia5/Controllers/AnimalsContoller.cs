750912c063f3123e0491408f0220bc35f15a8cee
s28691 <s28691@pjwstk.edu.pl>
2024-04-16 12:59:30 +0200

    Connection configuration

--- apbd19-cwiczenia5/apbd19-cwiczenia5/Controllers/AnimalsContoller.cs
+++ apbd19-cwiczenia5/apbd19-cwiczenia5/Controllers/AnimalsContoller.cs
@@ -1,13 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace apbd19_cwiczenia5.Controllers;
 [ApiController]
 [Route("api/[controller]")]
 public class AnimalsContoller : ControllerBase
 {
+    private readonly IConfiguration _configuration;
+    public AnimalsContoller(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
     [HttpGet]
     public IActionResult GetAnimals()
     {
+        SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("Default"));
+        connection.Open();
         return Ok();
     }
 }
